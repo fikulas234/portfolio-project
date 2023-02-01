dc6839a5f4740008c3373078e086f9d4fe4fc94a
fikulas234 <trbojevicfilip96@gmail.com>
2023-02-01 21:03:09 +0100

    style changes

--- components/right-content/homepage.js
+++ components/right-content/homepage.js
@@ -6,7 +6,7 @@ const HomePageContent = () => {
       <h1>Welcome to my portfolio website</h1>
       <p>
         The idea for this portfolio was that it has the looks of some kind
-        'file-based' system where you can navigate through it. I just started
+        "file-based" system where you can navigate through it. I just started
         learning Next. JS and this was a great opportunity to make something
         practical with the newly acquired knowledge. It was a fun experience and
         it has helped me to understand some of this framework concepts and why
