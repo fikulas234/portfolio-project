c669d096748e00cac6e582422bf55a0e2cb054aa
Filip Trbojevic <trbojevicfilip96@gmail.com>
2023-02-13 19:55:24 +0100

    style changes

--- components/right-content/resources.js
+++ components/right-content/resources.js
@@ -1,12 +1,62 @@
 import RightSideContent from "./main-content";
 import Card from "../layout/card";
 import NavigationElements from "../left-navigation/navigation";
+import classes from "./resources.module.css";
+import Link from "next/link";
 
 const ResourcesContent = () => {
   return (
     <Card>
       <NavigationElements />
-      <RightSideContent>Resources Content</RightSideContent>
+      <RightSideContent>
+        <div className={classes.content}>
+          <h1>Resources</h1>
+          <p>
+            Here you will find resources I have used while learning front-end
+            technologies. Maybe it will be usefull for someone starting learning
+            journey so I will leave sources where you can find resources and
+            courses.
+          </p>
+          <p>
+            <Link href="https://www.theodinproject.com/" target="_blank">
+              www.theodinproject.com
+            </Link>{" "}
+            - overall, one of the best free resources for learning front-end. It
+            also has very nice and helpfull community!
+          </p>
+          <p>
+            <Link
+              href="https://www.udemy.com/course/the-complete-javascript-course/?utm_campaign=website1010&utm_medium=website1010&utm_source=mycoupon"
+              target="_blank"
+            >
+              The Complete JavaScript Course 2023: From Zero to Expert!
+            </Link>{" "}
+            - personally I have found that Jonas Schmedtmann has the best
+            approach at teaching complicated concepts so it is easy to
+            understand.
+          </p>
+          <p>
+            <Link
+              href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/?couponCode=D_0223"
+              target="_blank"
+            >
+              React.js course
+            </Link>{" "}
+            - great React.js course from Maximilian Schwarzmüller who is also a
+            great teacher.
+          </p>
+          <p>
+            <Link
+              href="https://www.udemy.com/course/nextjs-react-the-complete-guide/?couponCode=D_0223"
+              target="_blank"
+            >
+              Next.js course
+            </Link>{" "}
+            - another great course from Maximilian Schwarzmüller but for
+            Next.js.
+          </p>
+        </div>
+      </RightSideContent>
     </Card>
   );
 };
